92a57fd7c422525303f7cac63dffbe28c63311de
Kevin Arnall <kevinjarnall@gmail.com>
2019-10-18 14:06:11 -0500

    added categories to db migration file

--- migration.sql
+++ migration.sql
@@ -2,6 +2,8 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+drop table if exists categories;
+drop table if exists ad_cat;
 
 CREATE TABLE users
 (
@@ -24,3 +26,17 @@ CREATE TABLE ads
     FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
 );
+
+create table categories
+(
+    id   int unsigned not null auto_increment primary key,
+    name varchar(50)
+);
+
+create table ad_cat
+(
+    ad_id  int unsigned not null,
+    cat_id int unsigned not null,
+    foreign key (ad_id) references ads (id),
+    foreign key (cat_id) references categories (id)
+)
